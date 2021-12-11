ffad1b5692a262321277c14df7ccf59500b77057
Haardispro <haardmajmudar2827@gmail.com>
2021-12-11 13:57:22 +0530

    added about

--- about.py
+++ about.py
@@ -15,10 +15,14 @@ heading = Label(q, text="Haardcode", fg="white", bg=bg, font=font)
 
 
 #Authors: Haard Majmudar
+authors_label = Label(q, text="Authors: Haard Majmudar", font=font, fg="white", bg=bg)
 #Version 0.0.1
-#Made on [insert date of finish]
+version_label = Label(q, text="Version: 0.0.1", font=font, bg=bg, fg="white")
+
 
 #positions
 heading.grid(row=0, column=0, padx=230, pady=10)
+authors_label.grid(row=1, column=0, pady=10)#, padx=230, pady=10)
+version_label.grid(row=2, column=0, pady=10)#, padx=230, pady=10)
 
 q.mainloop()
