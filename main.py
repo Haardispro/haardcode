446a3ac214222ecb363bb1740f72ac2ceaf7903f
Folgue02 <davidfolru@gmail.com>
2022-07-07 12:38:28 +0200

    Better spacing and themes implementations

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from tkinter.filedialog import *
 from tkinter import messagebox
 import tkinter.font
 import os
-import platform 
+import platform
+import theme
+
+# Colors
+GLOBAL_THEME = theme.get_theme_from_dict(theme.DEFAULT_THEME_DICT)
 
 
 w = Tk()
@@ -25,12 +29,16 @@ def save_as_file():
     text2save=str(main_text.get(1.0, END))
     f.write(text2save)
     f.close()
+
+
 def open_file():
     open_file.filename = askopenfile(mode = "r")
     if open_file.filename is not None:
         content = open_file.filename.read()
         main_text.delete(0.0, END)
         main_text.insert(END, content) 
+
+
 def save():
     try:
         if save_as_file.name is not None:
@@ -48,14 +56,17 @@ def save():
     except AttributeError:
         save_as_file()
 
+
 def exit():
     w.destroy()
+
 
 def new():
     main_text.delete(0.0, END)
     w.title("Haardcode")
 
-#Shitty code 
+
+# Shitty code 
 """
 def preferences():
     os.system("python preferences.py")
@@ -63,11 +74,12 @@ def preferences():
 def about():
     os.system("python about.py")
 
-#printing
-def print_file(): 
+
+def print_file():
+    """Printing"""
     a = Tk()
     a.title("Print")
-    a.configure(bg="#282828")
+    a.configure(bg=GLOBAL_THEME.background)
     a.resizable(width=False, height=False)
     font=("Cascadia Code", 14)
     operating_system = platform.system()
@@ -82,7 +94,6 @@ def print_file():
                 if open_file.filename is not None:
                     os.system("lp {}".format(open_file.filename.name))
                     a.destroy()
-       
         elif operating_system == 'Windows':
             try:
                 if save_as_file.name is not None:
@@ -91,39 +102,95 @@ def print_file():
             except AttributeError:
                 if open_file.filename is not None:
                     os.system("notepad /p {}".format(open_file.filename.name))
-       
-    #Before printing set default printer
-    heading = Label(a, text="Printing options", font=font, fg="white", bg="#282828")
-    note = Label(a, text="Note: Before you print, please set your default printer", font=font, fg="white", bg="#282828")
-    #normal button
+    # Before printing set default printer
+    heading = Label(a, text="Printing options", font=font, fg="white", bg=GLOBAL_THEME.background)
+    note = Label(a, text="Note: Before you print, please set your default printer", font=font, fg="white", bg=GLOBAL_THEME.background)
+    # normal button
     normal = Button(a, text="Normal Print", font=font, command=normal_print)
-    #positions
+    # positions
     heading.grid(row=0, column=0, padx=10, pady=10)
     note.grid(row=1, column=0, padx=10, pady=10)
     normal.grid(row=2, column=0, padx=10, pady=10)
     a.mainloop()
 
-#Font
-font = tkinter.font.Font(family='Cascadia Code', size=14)
+# Font
+font = tkinter.font.Font(family=GLOBAL_THEME.font, size=GLOBAL_THEME.font_size)
 
-#Menubar
-menubar=Menu(w, font=font, bg="#282828", fg="#f8f8f2", relief=FLAT)
+# Menubar
+menubar=Menu(w, font=font, bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground, relief=FLAT)
 
-#Files tab
-files = Menu(menubar, tearoff=0, font=font, bg="#282828", fg="#f8f8f2", relief=FLAT)
+# Files tab
+files = Menu(menubar, tearoff=0, font=font, bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground, relief=FLAT)
 files.add_command(label="New", command=new)
 files.add_command(label="Open", command=open_file)
 files.add_command(label="Save", command=save)
-files.add_command(label="Save as..", command=lambda:save_as_file())
+files.add_command(label="Save as..", command=lambda: save_as_file())
 files.add_command(label="Print", command=print_file)
 files.add_separator()
 files.add_command(label="Exit", command=exit)
 menubar.add_cascade(label="File", menu=files)
 
-#Edit
-edit = Menu(menubar, tearoff=0, font=font, bg="#282828", fg="#f8f8f2", relief=FLAT)
+main_text = Text(w, width=800, height=600, wrap=WORD, font=font, bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground, insertbackground='white', highlightthickness=0, relief=SOLID)
+
+# TODO: Continue here:
+#   - Theme loader function
+#   - Implement themes.py in here
+def pick_theme():
+    from tkinter import ttk
+    theme.initialize_themes_folder()
+
+    def load_theme():
+        theme_name = clicked.get()
+        new_editor_theme = theme.load_theme(theme_name)
+        # Change the global theme var
+        global GLOBAL_THEME
+        GLOBAL_THEME = new_editor_theme
+        font = tkinter.font.Font(family=GLOBAL_THEME.font, size=GLOBAL_THEME.font_size)
+
+        # Apply the changes
+        menubar.configure(
+                bg=GLOBAL_THEME.background,
+                fg=GLOBAL_THEME.foreground,
+                font=font)
+        edit.configure(
+                bg=GLOBAL_THEME.background,
+                fg=GLOBAL_THEME.foreground,
+                font=font)
+        files.configure(
+                bg=GLOBAL_THEME.background,
+                fg=GLOBAL_THEME.foreground,
+                font=font)
+        main_text.configure(
+                bg=GLOBAL_THEME.background,
+                fg=GLOBAL_THEME.foreground,
+                font=font)
+
+        print("Theme changed.")
+        theme_window.destroy()
+
+    # Available themes in themes folder
+    themes = theme.list_themes()
+    theme_window = Toplevel(w)
+    #theme_window.configure(bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground)
+    #theme_window.geometry("400x200")
+    clicked = StringVar()
+    clicked.set(themes[0])
+
+    frame = Frame(theme_window)
+    options = OptionMenu(frame, clicked, *themes)
+    load_btn = Button(frame, text="Load theme", command=load_theme)
+    load_btn.configure(bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground)
+    options.pack()
+    load_btn.pack()
+    frame.grid(sticky="ne")
+    theme_window.title("Theme selector")
+
+
+# Edit
+edit = Menu(menubar, tearoff=0, font=font, bg=GLOBAL_THEME.background, fg=GLOBAL_THEME.foreground, relief=FLAT)
 #edit.add_command(label="Preferences", command=preferences)
-#menubar.add_cascade(label="Edit", menu=edit)
+edit.add_command(label="themes", command=pick_theme)
+menubar.add_cascade(label="Edit", menu=edit)
 
 #About
 menubar.add_command(label="About", command=about)
@@ -132,7 +199,6 @@ menubar.add_command(label="About", command=about)
 menubar.add_command(label="Help")
 
 #Main Text Box
-main_text = Text(w, width=800, height=600, wrap=WORD, font=font, bg="#282828", fg="#f8f8f2", insertbackground='white', highlightthickness=0, relief=SOLID)
 
 #positions
 main_text.grid(row=1, column=0)
